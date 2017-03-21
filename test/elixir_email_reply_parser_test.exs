623a639f76796062f26472fcb6952a93c6e3e7d7
slavo <slavo2@users.noreply.github.com>
2017-03-21 23:54:58 +0100

    First test stub / draft

--- test/elixir_email_reply_parser_test.exs
+++ test/elixir_email_reply_parser_test.exs
@@ -5,4 +5,15 @@ defmodule ElixirEmailReplyParserTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "test_simple_body" do
+    {:ok, message} = get_email('email_1_1')
+  end
+
+
+  defp get_email(name) do
+    {:ok, content} = File.open("test/emails/#{name}.txt")
+    ElixirEmailReplyParser.read(content)
+  end
+
 end
