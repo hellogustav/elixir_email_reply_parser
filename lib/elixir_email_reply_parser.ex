769087b6b7f7f0dfe1b27ace5bfb74c05bf0c2e9
slavo <slavo2@users.noreply.github.com>
2017-03-23 00:02:24 +0100

    ElixirEmailReplyParser: method: read(text) structs: EmailMessage, Fragment submodule stubs: Parser

--- lib/elixir_email_reply_parser.ex
+++ lib/elixir_email_reply_parser.ex
@@ -15,4 +15,33 @@ defmodule ElixirEmailReplyParser do
   def hello do
     :world
   end
+
+  def read(text) do
+    ElixirEmailReplyParser.Parser.read(text)
+  end
+end
+
+defmodule ElixirEmailReplyParser.Fragment do
+  defstruct [
+    signature: false,
+    headers: false,
+    hidden: false,
+    quoted: false,
+    content: nil,
+    lines: []
+  ]
+end
+
+defmodule ElixirEmailReplyParser.EmailMessage do
+  defstruct [
+    fragments: []
+  ]
+end
+
+defmodule ElixirEmailReplyParser.Parser do
+  def read(text) do
+    lines = String.split(text, "\n")
+    fragments = for line <- lines, do: %ElixirEmailReplyParser.Fragment{content: "#{line}"}
+    %ElixirEmailReplyParser.EmailMessage{fragments: fragments}
+  end
 end
