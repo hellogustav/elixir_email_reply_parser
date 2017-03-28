5ab4d2c59867e860f2c2059b3e702b5334601ab8
slavo <slavo2@users.noreply.github.com>
2017-03-28 18:24:10 +0200

    mix.exs temporary update - removal of elixir version constraint

--- mix.exs
+++ mix.exs
@@ -4,7 +4,6 @@ defmodule ElixirEmailReplyParser.Mixfile do
   def project do
     [app: :elixir_email_reply_parser,
      version: "0.1.0",
-     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
