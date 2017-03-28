5b7e15775291f1a3a474007ee0747e92402a6305
slavo <slavo2@users.noreply.github.com>
2017-03-28 05:55:50 +0200

    Functionality draft passing simple test (however with warnings on compilation)

--- lib/elixir_email_reply_parser.ex
+++ lib/elixir_email_reply_parser.ex
@@ -59,19 +59,84 @@ defmodule ElixirEmailReplyParser.Parser do
     text = Regex.replace(~r/([^\n])(?=\n_{7}_+)$/m, text, "\\1\n")
     lines = String.split(text, "\n")
     lines = Enum.reverse(lines)
-    {:ok, fragments} = scan_line([], nil, lines)
+    {:ok, fragments} = scan_line([], false, nil, lines)
 
     %ElixirEmailReplyParser.EmailMessage{fragments: Enum.reverse(fragments)}
   end
 
-  defp scan_line(fragments, nil, []), do: {:ok, fragments}
-
-  defp scan_line(fragments, nil, [line | lines]) do
-    fragment = %ElixirEmailReplyParser.Fragment{content: "#{line}"}
-    scan_line([fragment | fragments], nil, lines)
+  defp string_empty?(s) when is_bitstring(s) do
+    String.trim(s) === ""
   end
 
-  defp scan_line(fragments, fragment, lines) do
-    scan_line([fragment | fragments], nil, lines)
+  defp scan_line(fragments, _found_visible, nil, []), do: {:ok, fragments}
+
+  defp scan_line(fragments, found_visible, fragment, []) do
+    if (fragment) do
+      fragment = %{fragment | content: Enum.join(fragment.lines), lines: nil}
+      if (fragment.headers) do
+        found_visible = false
+        fragments = for frag <- fragments, do: %{frag | hidden: true}
+      end
+      unless (found_visible) do
+        if (fragment.quoted or fragment.headers or fragment.signature or (string_empty?(fragment.content))) do
+          fragment = %{fragment | hidden: true}
+        else
+          found_visible = true
+        end
+      end
+      fragments = [fragment | fragments]
+    end
+    scan_line(fragments, found_visible, nil, [])
+  end
+
+  defp scan_line(fragments, found_visible, fragment, [line | lines]) do
+    is_quoted = Regex.match?(~r/^ *(>+)/, line)
+    is_quote_header = Regex.match?(~r/On.*wrote:$/, line)
+    is_header = is_quote_header or Regex.match?(~r/^(From|Sent|To|Subject): .+/, line)
+    is_empty = string_empty?(line)
+
+    if (fragment && is_empty) do
+      [previous_line | _tail] = fragment.lines
+      previous_line = String.trim(previous_line)
+      if (Regex.match?(~r/(--|__|-\w)|(^Sent from my (\w+\s*){1,3})/, previous_line)) do
+        fragment = %{fragment | content: Enum.join(fragment.lines), signature: true, lines: nil}
+        if (fragment.headers) do
+          found_visible = false
+          fragments = for frag <- fragments, do: %{frag | hidden: true}
+        end
+        unless (found_visible) do
+          if (fragment.quoted or fragment.headers or fragment.signature or (string_empty?(fragment.content))) do
+            fragment = %{fragment | hidden: true}
+          else
+            found_visible = true
+          end
+        end
+        fragments = [fragment | fragments]
+        fragment = nil
+      end
+    end
+
+    if (fragment && (((fragment.headers == is_header) and (fragment.quoted == is_quoted)) or (fragment.quoted and (is_quote_header or is_empty)))) do
+      fragment = %{fragment | lines: [line | fragment.lines]}
+    else
+      if (fragment) do
+        fragment = %{fragment | content: Enum.join(fragment.lines), lines: nil}
+        if (fragment.headers) do
+          found_visible = false
+          fragments = for frag <- fragments, do: %{frag | hidden: true}
+        end
+        unless (found_visible) do
+          if (fragment.quoted or fragment.headers or fragment.signature or (string_empty?(fragment.content))) do
+            fragment = %{fragment | hidden: true}
+          else
+            found_visible = true
+          end
+        end
+        fragments = [fragment | fragments]
+        fragment = nil
+      end
+      fragment = %ElixirEmailReplyParser.Fragment{lines: [line], quoted: is_quoted, headers: is_header}
+    end
+    scan_line(fragments, found_visible, fragment, lines)
   end
 end
