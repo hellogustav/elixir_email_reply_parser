e868cb65e72ea5587a9b40065c7737f2f77358ed
slavo <slavo2@users.noreply.github.com>
2017-03-28 03:12:04 +0200

    ElixirEmailReplyParser.read(text) draft of some initials regular expression replacements

--- lib/elixir_email_reply_parser.ex
+++ lib/elixir_email_reply_parser.ex
@@ -40,6 +40,23 @@ end
 
 defmodule ElixirEmailReplyParser.Parser do
   def read(text) do
+    # Normalize line endings.
+    text = String.replace(text, "\r\n", "\n")
+
+    # Check for multi-line reply headers. Some clients break up
+    # the "On DATE, NAME <EMAIL> wrote:" line into multiple lines.
+    text = if (Regex.match?(~r/^(?!On.*On\s.+?wrote:)(On\s(.+?)wrote:)$/m, text)) do
+      # Remove all new lines from the reply header.
+      Regex.replace(~r/^(?!On.*On\s.+?wrote:)(On\s(.+?)wrote:)$/, text, fn x -> String.replace(x, "\n", "") end)
+    else
+      text
+    end
+
+    # Some users may reply directly above a line of underscores.
+    # In order to ensure that these fragments are split correctly,
+    # make sure that all lines of underscores are preceded by
+    # at least two newline characters.
+    text = Regex.replace(~r/([^\n])(?=\n_{7}_+)$/m, text, "\\1\n")
     lines = String.split(text, "\n")
     fragments = for line <- lines, do: %ElixirEmailReplyParser.Fragment{content: "#{line}"}
     %ElixirEmailReplyParser.EmailMessage{fragments: fragments}
