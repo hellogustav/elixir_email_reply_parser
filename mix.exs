36f1dd6e25c331db5c615b7bd603d599bfcfc767
slavo <slavo2@users.noreply.github.com>
2017-03-28 18:53:14 +0200

    mix.exs and .travis.yml update - requiring / testing for version 1.3 and above

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule ElixirEmailReplyParser.Mixfile do
   def project do
     [app: :elixir_email_reply_parser,
      version: "0.1.0",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
