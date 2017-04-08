c9ead95cfeeb1f7421b74eeaeb6c07850a0b9792
slavo <slavo2@users.noreply.github.com>
2017-04-08 16:02:06 +0200

    Removal of example test "the truth"

--- test/elixir_email_reply_parser_test.exs
+++ test/elixir_email_reply_parser_test.exs
@@ -2,10 +2,6 @@ defmodule ElixirEmailReplyParserTest do
   use ExUnit.Case
   doctest ElixirEmailReplyParser
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-
   test "test_simple_body" do
     email_message = get_email('email_1_1')
     %ElixirEmailReplyParser.EmailMessage{fragments: fragments} = email_message
