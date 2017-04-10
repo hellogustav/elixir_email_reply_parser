58cfa5c498d6bd60fd2578179a28b165ca0bf497
slavo <slavo2@users.noreply.github.com>
2017-04-10 19:01:00 +0200

    Refactoring / fix: using spec for type hinting instead of when guards

--- lib/elixir_email_reply_parser.ex
+++ lib/elixir_email_reply_parser.ex
@@ -73,13 +73,15 @@ defmodule ElixirEmailReplyParser.Parser do
     |> Enum.join("\n")
   end
 
-  defp normalize_line_endings(s) when is_binary(s) do
+  @spec normalize_line_endings(String.t) :: String.t
+  defp normalize_line_endings(s) do
     String.replace(s, "\r\n", "\n")
   end
 
   # Check for multi-line reply headers. Some clients break up
   # the "On DATE, NAME <EMAIL> wrote:" line into multiple lines.
-  defp handle_multiline(s) when is_binary(s) do
+  @spec handle_multiline(String.t) :: String.t
+  defp handle_multiline(s) do
     re = ~r/(?!On.*On\s.+?wrote:)(On\s(.+?)wrote:)/s
 
     if (Regex.match?(re, s)) do
@@ -94,27 +96,33 @@ defmodule ElixirEmailReplyParser.Parser do
   # In order to ensure that these fragments are split correctly,
   # make sure that all lines of underscores are preceded by
   # at least two newline characters.
-  defp draw_away_lines_with_underscores(s) when is_binary(s) do
+  @spec draw_away_lines_with_underscores(String.t) :: String.t
+  defp draw_away_lines_with_underscores(s) do
     Regex.replace(~r/([^\n])(?=\n_{7}_+)$/m, s, "\\1\n")
   end
 
-  defp string_empty?(s) when is_binary(s) do
+  @spec string_empty?(String.t) :: boolean
+  defp string_empty?(s) do
     String.trim(s) == ""
   end
 
-  defp string_signature?(s) when is_binary(s) do
+  @spec string_signature?(String.t) :: boolean
+  defp string_signature?(s) do
     Regex.match?(~r/(^\s*--|^\s*__|^-\w)|(^Sent from my (\w+\s*){1,3})/, s)
   end
 
-  defp string_quoted?(s) when is_binary(s) do
+  @spec string_quoted?(String.t) :: boolean
+  defp string_quoted?(s) do
     Regex.match?(~r/^ *(>+)/, s)
   end
 
-  defp string_quote_header?(s) when is_binary(s) do
+  @spec string_quote_header?(String.t) :: boolean
+  defp string_quote_header?(s) do
     Regex.match?(~r/On.*wrote:$/, s)
   end
 
-  defp string_email_header?(s) when is_binary(s) do
+  @spec string_email_header?(String.t) :: boolean
+  defp string_email_header?(s) do
     Regex.match?(~r/^(From|Sent|To|Subject): .+/, s)
   end
 
